444d9d2504d8618a9386eaeefafc0b51f9e06c1c
Cainiaotao <209170072@qq.com>
2019-04-25 23:55:08 +0800

    ++++++++自定义验证码输入框

--- app/src/main/java/com/tantao/wxdemo/ui/activity/RegisterActivity.kt
+++ app/src/main/java/com/tantao/wxdemo/ui/activity/RegisterActivity.kt
@@ -1,9 +1,12 @@
 package com.tantao.wxdemo.ui.activity
 
+import android.os.Bundle
 import com.tantao.wxdemo.R
 import com.tantao.wxdemo.presenter.RegisterPresenter
 import com.tantao.wxdemo.presenter.contract.RegisterContract
 import com.tantao.wxdemo.ui.base.BaseActivity
+import com.tantao.wxdemo.ui.widget.VerifyCodeView
+import kotlinx.android.synthetic.main.activity_register.*
 
 /**
  * @deprecated：文件描述
@@ -11,13 +14,33 @@ import com.tantao.wxdemo.ui.base.BaseActivity
  * @date：2019/3/22
  *
  */
-class RegisterActivity:BaseActivity<RegisterContract.IView,RegisterPresenter>(),RegisterContract.IView {
+class RegisterActivity:BaseActivity<RegisterContract.IView,RegisterPresenter>(),RegisterContract.IView, VerifyCodeView.InputCompleteListener {
 
     override fun setContentView(): Int = R.layout.activity_register
 
     override fun createPresenter(): RegisterPresenter  = RegisterPresenter(this)
 
     override fun onRegisterResult() {
+
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
+        super.initView(savedInstanceState)
+    }
+
+
+    override fun initData() {
+        super.initData()
+
+        et_verify.setCompleteListener(this)
+    }
+
+
+    override fun onComplete(string: String) {
+       showToast(this,string)
+    }
+
+    override fun invalidContent() {
 
     }
 }
